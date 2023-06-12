c2efce6458133be290222b87275f7c9ccc479e00
alvaroRodriguez92 <alvaros.rodriguez92@gmail.com>
2023-06-12 20:50:07 +0200

    añadido primer formulario  y wizard model

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import Map from "./views/Map/Map";
 import "./App.css";
 import UserContextProvider from "./context/UserContext";
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Registro from "./components/Registro/Registro";
 
 function App() {
   return (
+    <BrowserRouter>
     <UserContextProvider>
-      <Map />
+    <Routes>
+      <Route path="/map" element={<Map/>} />
+      <Route path="/registro" element={<Registro/>} />
+    </Routes>
     </UserContextProvider>
+    </BrowserRouter>
   );
 }
 
