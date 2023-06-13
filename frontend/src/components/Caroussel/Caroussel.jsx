651e28b1366f0d2a655bc0959b515893788e4aec
Estela Extremera <estelaextremeracampos@gmail.com>
2023-06-13 09:24:26 +0200

    change caroussel

--- frontend/src/components/Caroussel/Caroussel.jsx
+++ frontend/src/components/Caroussel/Caroussel.jsx
@@ -1,27 +1,74 @@
-import Carousel from "react-bootstrap/Carousel";
-
 export default function Caroussel() {
-  let images = [
-    { nombre: "ganaderia", src: "../../src/assets/ganaderia.jpg" },
-    { nombre: "pesca", src: "../../src/assets/pesca.jpg" },
-    {
-      nombre: "explotacion forestal",
-      src: "../../src/assets/xplotacion forestal.png",
-    },
-  ];
   return (
-    <>
-      <Carousel>
-        {images?.map((image) => (
-          <Carousel.Item>
-            <img className="imagen-slider" src={image.src} alt="Second slide" />
-            <Carousel.Caption>
-              <h3>Las mejores peliculas del 2023</h3>
-              <p>a un boton de tenerlas en casa</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    </>
+    
+    <div
+      id="carouselExampleCaptions"
+      class="carousel slide"
+      data-bs-ride="carousel"
+    >
+      <div class="carousel-indicators">
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="0"
+          class="active"
+          aria-current="true"
+          aria-label="Slide 1"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="1"
+          aria-label="Slide 2"
+        ></button>
+        <button
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide-to="2"
+          aria-label="Slide 3"
+        ></button>
+      </div>
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <img src="..." class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>First slide label</h5>
+            <p>Some representative placeholder content for the first slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Second slide label</h5>
+            <p>Some representative placeholder content for the second slide.</p>
+          </div>
+        </div>
+        <div class="carousel-item">
+          <img src="..." class="d-block w-100" alt="..." />
+          <div class="carousel-caption d-none d-md-block">
+            <h5>Third slide label</h5>
+            <p>Some representative placeholder content for the third slide.</p>
+          </div>
+        </div>
+      </div>
+      <button
+        class="carousel-control-prev"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="prev"
+      >
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+      </button>
+      <button
+        class="carousel-control-next"
+        type="button"
+        data-bs-target="#carouselExampleCaptions"
+        data-bs-slide="next"
+      >
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+      </button>
+    </div>
   );
 }
