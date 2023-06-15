0b4191d5dc5df040057463f010a7535260688f7c
Estela <estelaextremeracampos@gmail.com>
2023-06-15 17:38:30 +0200

    cambios perfil

--- frontend/src/views/Perfil/Perfil.jsx
+++ frontend/src/views/Perfil/Perfil.jsx
@@ -3,13 +3,12 @@ import { Box } from '@mui/material';
 export default function Perfil() {
 
     return (
-        <>
+        
         <Box >
             <img src="../../src/assets/enelpsicologo.jpeg" />
-            <Box  sx={{ height: "10px" }}>
-                <img src="../../src/assets/logonuevamente.png" />
-            </Box>
+                <img lassName="imagenView" src="../../src/assets/logonuevamente.png" />
+           
         </Box>
-        </>
+        
     )
 }
