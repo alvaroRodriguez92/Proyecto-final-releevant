a5097d203617555c3692f483559dd3fed9491ca5
FRANCIS <110781698+Foxxhaund@users.noreply.github.com>
2023-07-10 17:23:41 +0200

    flyto y modificado valorController

--- frontend/src/components/CardEmpresas/CardEmpresas.jsx
+++ frontend/src/components/CardEmpresas/CardEmpresas.jsx
@@ -34,7 +34,6 @@ export default function CardEmpresas() {
     return distancia;
   }
   function handleClick(id) {
-    console.log(id,"id del perfil")
     setPerfilCompleto(id);
   }
   if (!empresas.length) return <></>;
